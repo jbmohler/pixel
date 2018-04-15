1c9f1b6ad06b0b6e3bd9d9c1759595c35197689e
Joel B. Mohler <joel@kiwistrawberry.us>
2018-04-15 15:20:32 -0400

    auto-refreshing page and blank initial image

--- server/application.py
+++ server/application.py
@@ -29,13 +29,16 @@ def api_long():
     config = get_configuration()
     return 'Hello World -- {}!'.format(config['name'])
 
+@app.route('/api/blank-image', methods=['PUT'])
+def put_api_blank_image():
+    app._image = np.zeros((10, 10), dtype=int)
+
 @app.route('/api/pixel', methods=['PUT'])
 def put_api_pixel():
     x = int(request.form['x'])
     y = int(request.form['y'])
     value = request.form['value']
 
-    app._image = np.zeros((10, 10), dtype=int)
     app._image[x, y] = 1
     return 'assigned', 200
 
@@ -52,6 +55,20 @@ def get_pixel_image():
     headers = {'Content-Type': 'image/png'}
     return content, 200, headers
 
+@app.route('/current.html')
+def get_current_html():
+    return """
+<html>
+ <head>
+  <meta http-equiv="refresh" content="3">
+ </head>
+ <body>
+ <img src='/pixel-image.png' alt='the current image' />
+ </body>
+</html>"""
+
 if __name__ == '__main__':
+    put_api_blank_image()
+
     config = get_configuration()
     app.run(port=config['port'])
